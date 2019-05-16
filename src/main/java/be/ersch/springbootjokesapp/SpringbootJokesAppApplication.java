337c8f178e48055448a2afd154a7d4e809ecd7fe
ersch <8956871+Ersch@users.noreply.github.com>
2019-05-16 22:21:40 +0200

    refactoring to test a spring config xml instead of spring configuration java class

--- src/main/java/be/ersch/springbootjokesapp/SpringbootJokesAppApplication.java
+++ src/main/java/be/ersch/springbootjokesapp/SpringbootJokesAppApplication.java
@@ -2,8 +2,10 @@ package be.ersch.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:jokeApp-config.xml")
 public class SpringbootJokesAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/be/ersch/springbootjokesapp/config/JockesConfiguration.java
+++ src/main/java/be/ersch/springbootjokesapp/config/JockesConfiguration.java
@@ -4,12 +4,12 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class JockesConfiguration {
 
-    @Bean
-    public ChuckNorrisQuotes chuckNorrisQuotes(){
-        return new ChuckNorrisQuotes();
-    }
+//    @Bean
+//    public ChuckNorrisQuotes chuckNorrisQuotes(){
+//        return new ChuckNorrisQuotes();
+//    }
 
 }
